2b60c9441c1c6fd37d219836e007af8cfedf3335
Benjamin Rouilly <benjamin.rouilly@gmail.com>
2023-04-18 23:12:18 +0200

    collapse about code ok, css à faire

--- kasa/src/pages/About.jsx
+++ kasa/src/pages/About.jsx
@@ -1,37 +1,60 @@
 import React from 'react'
 import BannerAbout from '../assets/imgapropos.png'
-import HousingDescription from '../components/Collapse'
+import CollapsibleTemplate from '../components/Collapse'
 import"./About.scss";
 
 function About() {
-  const aproposData = [
-      {
-          title:'Fiabilité',
-          content:"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
-      },
-      {
-          title:'Respect',
-          content:"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-      },
-      {
-          title:'Service',
-          content:"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-      },
-      {
-          title:'Sécurité',
-          content:"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-      }
-  ];
 
   return (
       <>
           <div className="bannerAbout">
                   <img src={BannerAbout} className="App-logo" alt="logo" />
           </div>
-          <div className='collapesCollection'>
-          {aproposData.map(({ title, content }, index) => (
-              <HousingDescription title={title} content={content} key={index} />                    
-          ))}</div>
+          <div className="testcolonne">
+            <CollapsibleTemplate label="Fiabilité" fontSize={24}>
+               <p className="description_text_about">
+                  Chez Kasa, nous sommes fiers d'offrir un service de
+                  réservation d'hôtels fiable et de qualité supérieure. Nous
+                  nous engageons à honorer chaque réservation et à fournir des
+                  informations précises et à jour sur chaque établissement.
+               </p>
+            </CollapsibleTemplate>
+
+            <CollapsibleTemplate label="Respect" fontSize={24}>
+               <p className="description_text_about">
+                  Nous croyons que chaque client mérite un service respectueux
+                  et attentif. C'est pourquoi nous nous efforçons de créer une
+                  expérience de réservation personnalisée et facile pour chaque
+                  client, en répondant à leurs besoins individuels et en leur
+                  offrant un service de haute qualité à chaque étape.
+               </p>
+            </CollapsibleTemplate>
+            <CollapsibleTemplate label="Service" fontSize={24}>
+               <p className="description_text_about">
+                  Notre objectif est de fournir un service exceptionnel à chaque
+                  client, à chaque fois. Nous nous engageons à offrir une
+                  assistance rapide et courtoise à tout moment, ainsi qu'à
+                  fournir des informations détaillées sur chaque hôtel, pour que
+                  nos clients puissent faire des choix éclairés en toute
+                  confiance.
+               </p>
+            </CollapsibleTemplate>
+            <CollapsibleTemplate
+               label="Responsabilité"
+               fontSize={24}
+               className="testsalut"
+            >
+               <p className="description_text_about">
+                  Nous sommes conscients de notre responsabilité envers nos
+                  clients et envers l'environnement. Nous nous engageons à
+                  fournir des informations précises et à jour sur chaque
+                  établissement, à respecter les normes éthiques et
+                  environnementales les plus strictes et à travailler avec des
+                  partenaires responsables pour assurer une expérience de voyage
+                  durable et respectueuse de l'environnement.
+               </p>
+            </CollapsibleTemplate>
+         </div>
 
       </>
   );
