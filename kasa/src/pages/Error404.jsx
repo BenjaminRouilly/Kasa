dd9bfa30cc1ae33916b7f01d42859c607f2ae576
Benjamin Rouilly <benjamin.rouilly@gmail.com>
2023-04-21 05:04:45 +0200

    affichage footer sur chaque page

--- kasa/src/pages/Error404.jsx
+++ kasa/src/pages/Error404.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import "../styles/Error404.scss";
 import NavBar from '../components/NavBar';
 import Main from '../components/Main';
+import Footer from '../components/Footer';
 
 function Error404() {
   return (
@@ -15,6 +16,7 @@ function Error404() {
                 <Link to="/">Retournez sur la page d'accueil</Link>
             </div>
         </Main>
+        <Footer/>
       </>
   )
 }
